2f427c52e57aba8b0f540641582d4e7ace8ad42b
Saul Ocampo <digitalh2o@localhost.localdomain>
2015-08-27 01:14:50 -0400

    first edits

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 16; y++) {
+            var square = $("<div class='square'></div>");
+            square.appendTo('#container');
+        }
+    }
+});
