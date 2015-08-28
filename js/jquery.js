dcba79ea7ac4903db9af77a1455a73214888bd2e
Saul Ocampo <digitalh2o@localhost.localdomain>
2015-08-28 00:05:29 -0400

    Random Colors Now

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,17 @@
+var rndColor = Math.floor(Math.random()*16777215).toString(16)
+
 $(document).ready(function() {
     for(var x = 0; x < 16; x++) {
         for(var y = 0; y < 16; y++) {
             var square = $("<div class='square'></div>");
             square.appendTo('#container');
-
-            $( ".square" ).hover(function() {
-  				$(this).stop(true, true).fadeToggle();
+		
+		$( ".square" ).on('mouseover',function() {
+  				$(this).css('background-color', rndColor);
 			});
+            
         }
     }
 });
+
 
