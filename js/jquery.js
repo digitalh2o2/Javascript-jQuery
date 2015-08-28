ebc4688fd57a96c24bf9eb358a627279681d7f1f
Saul Ocampo <digitalh2o@localhost.localdomain>
2015-08-27 22:18:20 -0400

    Hovering is working

--- js/jquery.js
+++ js/jquery.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
         for(var y = 0; y < 16; y++) {
             var square = $("<div class='square'></div>");
             square.appendTo('#container');
+
+            $( ".square" ).hover(function() {
+  				$(this).stop(true, true).fadeToggle();
+			});
         }
     }
 });
+
